7110bb1a19cf185dc388a4975bb4756fec58a339
cgrauss <50146004+cgrauss@users.noreply.github.com>
2019-10-19 21:31:23 -0500

    Add preview Project/Goal Info methods
    Get the preview info for Project and Goals. These are subject to change depending on what our frontend wants to see.

--- src/mothership/Mothership.java
+++ src/mothership/Mothership.java
@@ -43,6 +43,17 @@ public class Mothership extends sql_API_FAKE {
 		return goalInfo;
 	}
 	
+	public String[] getPreviewGoalInfo(String goalID) {
+		String[] goalInfo = new String[4];
+		goalInfo[0] = sql_API_FAKE.goalsgetName(goalID); //Name
+		String description	= sql_API_FAKE.goalsgetDescription(projectID);
+		goalInfo[1] = description.substring(0, 150);  //150 char description -- length can be changed
+		goalInfo[2] = sql_API_FAKE.goalsgetAssignee(goalID); //Assignee
+		goalInfo[3] = sql_API_FAKE.goalsgetRemainingDate(goalID); //Remaining Date
+		goalInfo[4] = sql_API_FAKE.goalsgetStatus(goalID); //Status
+		return goalInfo;
+	}
+	
 	public String[] getDetailedProjectInfo(String projectID) {
 		String[] projectInfo = new String[9];
 		projectInfo[0] = projectID;
@@ -55,6 +66,18 @@ public class Mothership extends sql_API_FAKE {
 		projectInfo[7] = sql_API_FAKE.projectsgetGroupID(projectID); //Group ID
 		projectInfo[8] = sql_API_FAKE.projectsgetStatus(projectID); //Status
 		projectInfo[9] = sql_API_FAKE.projectsgetGrade(projectID); //Grade
+		return projectInfo;
+	}
+	
+	public String[] getPreviewProjectInfo(String projectID) {
+		String[] projectInfo = new String[5];
+		projectInfo[0] = sql_API_FAKE.projectsgetName(projectID); //Name
+		String description	= sql_API_FAKE.projectsgetDescription(projectID);
+		projectInfo[1] = description.substring(0, 150);  //150 char description -- length can be changed
+		projectInfo[2] = sql_API_FAKE.projectsgetCreatorID(projectID); //Creator
+		projectInfo[3] = sql_API_FAKE.projectsgetRemainingDate(projectID); //Remaining Date
+		projectInfo[4] = sql_API_FAKE.projectsgetGroupID(projectID); //Group ID
+		projectInfo[5] = sql_API_FAKE.projectsgetStatus(projectID); //Status
 		return projectInfo;
 	}
 }
